db95c372622e3b56c14f71fa8cfca5f4c95e4025
Kedarpiyu <105104925+Kedarpiyu@users.noreply.github.com>
2022-05-07 14:52:25 +0100

    Update StepDefinitions.java
    Steps

--- src/main/java/com/clm/StepDefinitions.java
+++ src/main/java/com/clm/StepDefinitions.java
@@ -7,34 +7,37 @@ import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.json.JSONObject;
-import org.junit.Assert;
+import org.hamcrest.Matchers;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.Properties;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class StepDefinitions {
+
     RequestSpecification request;
-    @Given("^Login API is provided$")
-    public void login_API_is_provided() throws Exception {
+    Response response;
+
+    @Given("^user calling API for nace number (\\d+)$")
+    public void calling_getNaceDetails(int naceNumber) throws Exception {
         RestAssured.baseURI  = "http://dummy.restapiexample.com";
         request = RestAssured.given();
-
+        response = request.header("Content-Type", "application/json")
+                   .when().get("/api/v1/getNaceDetail/{naceNumber}",naceNumber);
     }
-    Response response;
-    @When("^User call login API$")
-    public void user_call_login_API() throws Exception {
-        request.header("Content-Type", "application/json");
-        response = request.get("/api/v1/employee/1");
-
-    }
-
-    @Then("^a token will be generated$")
-    public void a_token_will_be_generated() throws Exception {
+    
+    @When("^getNaceDetail API should return status 200$")
+    public void called_API_Status_Check() throws Exception {
         int statusCode = response.getStatusCode();
+        response.then().assertThat().statusCode(200);
         System.out.println(statusCode);
+    }
+
+    @Then("^Response should contains nace details$")
+    public void response_validation() throws Exception {
+        
         System.out.print(response.asString());
+        response.then().assertThat.body(Matchers.equalTo(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + 
+         "\\src\\test\\resources\\userDetail398481.json")))));
       }
 
  }
